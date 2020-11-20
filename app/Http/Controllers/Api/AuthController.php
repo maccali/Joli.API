2248a103232d91c36296ed13a04bb1ed1530576d
maccali <guilherme.maccali@universo.univates.br>
2020-11-20 00:20:33 -0300

    [FEAT] adição do usuário no retorno do login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,95 +4,97 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Validator;
 
 class AuthController extends Controller
 {
-        /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login']]);
+  /**
+   * Create a new AuthController instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth:api', ['except' => ['login']]);
+  }
+
+  /**
+   * Get a JWT via given credentials.
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function login(Request $request)
+  {
+
+    $data = json_decode($request->getContent(), true);
+
+    $validator = Validator::make(
+      $data,
+      [
+        'email' => 'required',
+        'password' => 'required',
+      ]
+    );
+
+    if ($validator->fails()) {
+      $messages = $validator->messages();
+      return response()->json([$messages], 400);
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login(Request $request)
-    {
-
-        $data = json_decode($request->getContent(), true);
-
-        $validator = Validator::make(
-            $data ,
-            [
-                'email' => 'required',
-                'password' => 'required',
-            ]
-        );
-
-        if ($validator->fails()){
-            $messages = $validator->messages();
-            return response()->json([$messages], 400);
-        }
-
-        if (! $token = auth('api')->attempt($data)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-
-        return $this->respondWithToken($token);
+    if (!$token = auth('api')->attempt($data)) {
+      return response()->json(['error' => 'Unauthorized'], 401);
     }
 
-    // /**
-    //  * Get the authenticated User.
-    //  *
-    //  * @return \Illuminate\Http\JsonResponse
-    //  */
-    // public function me()
-    // {
-    //     return response()->json(auth()->user());
-    // }
+    return $this->respondWithToken($token);
+  }
 
-    // /**
-    //  * Log the user out (Invalidate the token).
-    //  *
-    //  * @return \Illuminate\Http\JsonResponse
-    //  */
-    // public function logout()
-    // {
-    //     auth()->logout();
+  // /**
+  //  * Get the authenticated User.
+  //  *
+  //  * @return \Illuminate\Http\JsonResponse
+  //  */
+  // public function me()
+  // {
+  //     return response()->json(auth()->user());
+  // }
 
-    //     return response()->json(['message' => 'Successfully logged out']);
-    // }
+  // /**
+  //  * Log the user out (Invalidate the token).
+  //  *
+  //  * @return \Illuminate\Http\JsonResponse
+  //  */
+  // public function logout()
+  // {
+  //     auth()->logout();
 
-    // /**
-    //  * Refresh a token.
-    //  *
-    //  * @return \Illuminate\Http\JsonResponse
-    //  */
-    // public function refresh()
-    // {
-    //     return $this->respondWithToken(auth()->refresh());
-    // }
+  //     return response()->json(['message' => 'Successfully logged out']);
+  // }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
-        ]);
-    }
+  // /**
+  //  * Refresh a token.
+  //  *
+  //  * @return \Illuminate\Http\JsonResponse
+  //  */
+  // public function refresh()
+  // {
+  //     return $this->respondWithToken(auth()->refresh());
+  // }
+
+  /**
+   * Get the token array structure.
+   *
+   * @param  string $token
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  protected function respondWithToken($token)
+  {
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'bearer',
+      'expires_in' => auth('api')->factory()->getTTL() * 60,
+      'user' => auth('api')->user()
+    ]);
+  }
 }
